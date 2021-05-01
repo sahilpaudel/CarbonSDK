7c5790fd6cdaec92e585dd74c0731c1e0427c8a2
Sahil Paudel <sahil.p@pharmeasy.in>
2021-05-01 21:36:22 +0530

    fix sort by priority code

--- dist/evaluate.js
+++ dist/evaluate.js
@@ -181,7 +181,7 @@ function reduce_results(options, matched_rules) {
 
   matched_rules = options["matched_rules"];
   var sorted_matached_rules = matched_rules.sort(function (a, b) {
-    return a.priority < b.priority;
+    return b.priority - a.priority;
   });
 
   options["reduce_matched_rules"] = sorted_matached_rules.reduce(function (reduced_result, currentValue, _currentIndex, _oldArray) {
